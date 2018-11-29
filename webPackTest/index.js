6de300ab3ba7b063d1d5e182ea86843e9d334c1b
Karolis <karolissov@gmail.com>
2018-11-29 16:10:53 +0200

    Webpack testing

---
+++ webPackTest/index.js
@@ -0,0 +1,8 @@
+import moment from 'moment';
+
+console.log("Hello there from JavaScript Obi-wan!");
+console.log(moment().startOf('day').fromNow());
+
+let name = "Obi-wan", time = "today";
+console.log(`Hello ${name}, are you free for some droid hunt anytime ${time}?`);
+console.log(`ehhhhhhhhhhhhhhhhhhhhhhhhhh?`);
